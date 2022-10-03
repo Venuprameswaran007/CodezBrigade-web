b8843905ff08459d28edea54eb1d5da0b00352b8
Venuprameswaran007 <venu140998@gmail.com>
2022-10-03 12:56:17 +0530

    intial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# CodezBrigade-web
- 
---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h5>Welcome to CodezBrigade</h5>
+    </div>
+  );
+}
+
+export default App;
